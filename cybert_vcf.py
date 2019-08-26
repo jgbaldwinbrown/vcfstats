15f364a034570e6dcaeb8f134cc2367e765f90c9
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2019-08-26 17:28:00 -0600

    cybert working!

--- cybert_vcf.py
+++ cybert_vcf.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 
+import os
+import subprocess
+import tempfile
 import math
 import vcf
-import numpy as np
-import statsmodels.api as sm
 import sys
 import argparse
-import copy
 
 def parse_my_args():
     parser = argparse.ArgumentParser("Compute the Cochran-Mantel-Haenszel test on a VCF file")
     parser.add_argument("vcf", nargs="?", help="Input VCF file; default stdin")
     parser.add_argument("-c", "--control",  help="comma separated, 0-indexed VCF columns to use as controls; required.", required=True)
     parser.add_argument("-t", "--test",  help="comma separated, 0-indexed VCF columns to use as test data; required.", required=True)
+    parser.add_argument("-w", "--window_size",  help="Window size for sliding window cyber-t.", required=True)
 
     args = parser.parse_args()
     return(args)
@@ -27,44 +28,96 @@ def get_arg_vars(args):
     test = [int(x) for x in args.test.split(",")]
     if not len(control) == len(test):
         exit("unequal number of control and test pops!")
-    return((inconn, control, test))
+    window_size = args.window_size
+    return((inconn, control, test, window_size))
 
-def cmh_vcf(vcfin, control, test, outwriter):
-    tester = np.ndarray(shape=(2,2,len(control)))
-    for record in vcfin:
+def counts2af(count1, count2):
+    try:
+        out = float(count1) / (float(count1) + float(count2))
+    except ZeroDivisionError:
+        out = "NA"
+    return(out)
+
+def textify_records(records, control, test):
+    ncontrol = len(control)
+    ntest = len(test)
+    text = "\t".join(["c" + str(x) for x in range(ncontrol)] + ["t" + str(x) for x in range(ntest)]) + "\n"
+    for record in records:
+        afs = []
         calls = [call for call in record]
-        for i in range(len(control)):
-            tester[i,0,0] = calls[control[i]].data.AD[0]
-            tester[i,0,1] = calls[control[i]].data.AD[1]
-            tester[i,1,0] = calls[test[i]].data.AD[0]
-            tester[i,1,1] = calls[test[i]].data.AD[1]
-        cmh = sm.stats.StratifiedTable(tester)
-        writeout(cmh, record, outwriter)
-        #print(cmh.summary())
+        for i in control:
+            count1 = int(calls[i].data.AD[0])
+            count2 = int(calls[i].data.AD[1])
+            afs.append(counts2af(count1, count2))
+        for i in test:
+            count1 = int(calls[i].data.AD[0])
+            count2 = int(calls[i].data.AD[1])
+            afs.append(counts2af(count1, count2))
+        text = text + "\t".join(map(str,afs)) + "\n"
+    return(text)
 
-        #control_ad0, control_ad1 = [calls.AD[:2] for i in control]
-        #test_ad0, test_ad1 = [calls.AD[:2] for i in test]
+def cybert_vcf(vcfin, control, test, outwriter, window_size):
+    records = []
+    
+    for record in vcfin:
+        records.append(record)
+    
+    records_txt = textify_records(records, control, test)
+    
+    cybert_data = cybert_from_r(records_txt, len(control), len(test), window_size)
+    write_it(records, cybert_data, outwriter)
 
-def writeout(cmh, record, writer):
+def cybert_from_r(records_txt, control_count, test_count, window_size):
+    mytemp = tempfile.NamedTemporaryFile(mode="w+", delete=False)
+    mytempname = mytemp.name
+    for l in records_txt:
+        mytemp.write(l)
+    mytemp.close()
+    
+    out = subprocess.check_output([
+        "./run_cybert.R",
+        mytempname,
+        "-e", str(test_count),
+        "-c", str(control_count),
+        "-w", str(window_size)
+    ]).decode("utf-8")
+    
+    os.remove(mytempname)
+    return(out)
+
+def getpvals(cybert_data):
+    pvals = []
+    splitcyt = [x.split(' ') for x in cybert_data.split('\n')]
+    pcol = splitcyt[0].index('"pVal"') + 1
+    for l in splitcyt[1:]:
+        try:
+            pvals.append(float(l[pcol]))
+        except IndexError:
+            pass
+    return(pvals)
+
+def write_it(records, cybert_data, writer):
+    cybert_pvals = getpvals(cybert_data)
+    for record, cyberp in zip(records, cybert_pvals):
+        writeout(cyberp, record, writer)
+
+def writeout(cyberp, record, writer):
     #newrecord = copy.deepcopy(record)
-    record.INFO["CMH"] = str(cmh.test_null_odds().pvalue)
-    record.INFO["NLOG10CMH"] = str(-math.log10(cmh.test_null_odds().pvalue))
+    record.INFO["CYBERP"] = str(cyberp)
+    record.INFO["NLOG10CYBERP"] = str(-math.log10(cyberp))
     writer.write_record(record)
 
 def main():
     args = parse_my_args()
 
-    inconn, control, test = get_arg_vars(args)
+    inconn, control, test, window_size = get_arg_vars(args)
 
     vcfin = vcf.Reader(inconn)
     outwriter = vcf.Writer(sys.stdout, vcfin)
-    cmh_vcf(vcfin, control, test, outwriter)
+    cybert_vcf(vcfin, control, test, outwriter, window_size)
 
     inconn.close()
 
 if __name__ == "__main__":
     main()
 
-#>>> for i in b:
-#...     for j in i:
-#...         try:print(j.data.AD)
--- run_cybert.R
+++ run_cybert.R
@@ -2,4 +2,23 @@
 
 source("bayesreg.R")
 library(argparse)
+library(data.table)
 
+parser = ArgumentParser()
+parser$add_argument("table", help="data table to run cybert on")
+parser$add_argument("-c", "--controls", help="control columns for cybert")
+parser$add_argument("-e", "--experimental", help="experimental columns for cybert")
+parser$add_argument("-w", "--window_size", help="window size for cybert")
+
+args = parser$parse_args()
+
+control_cols = as.numeric(args$controls)
+experimental_cols = as.numeric(args$experimental)
+
+win_size = as.numeric(args$window_size)
+
+data = as.data.frame(fread(args$table, sep="\t", header=TRUE))
+
+odata = bayesT(aData=data, numC=control_cols, numE=experimental_cols, winSize=win_size)
+
+write.table(odata, "")
