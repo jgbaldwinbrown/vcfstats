83239bec61b5c9ebeea3109728abd0c97abbea6f
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2020-01-06 19:22:27 -0700

    install

---
+++ install.sh
@@ -0,0 +1,5 @@
+cp cmh_vcf_nolib.py ~/mybin/cmh_vcf_nolib
+cp cmh_vcf.py ~/mybin/cmh_vcf
+cp cmh_vcf_small_fast.py ~/mybin/cmh_vcf_small_fast
+cp cmh_vcf_small.py ~/mybin/cmh_vcf_small
+cp vcf-subset-fast.py ~/mybin/vcf-subset-fast
