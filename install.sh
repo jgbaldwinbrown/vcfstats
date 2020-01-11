119cfeba7a4045749cb260ffba4edc53714f0bef
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2020-01-10 17:36:03 -0700

    install updated

--- install.sh
+++ install.sh
@@ -12,3 +12,5 @@ cp plot_vcf_cmh_fast_wide.sh ~/mybin/plot_vcf_cmh_fast_wide
 cp plot_vcf_cmh_fast_wide_lowdpi.sh ~/mybin/plot_vcf_cmh_fast_wide_lowdpi
 cp pfst.sh ~/mybin/pfst
 cp vcf_colnums.py ~/mybin/vcf_colnums
+cp cybert_vcf_nolib.py ~/mybin/cybert_vcf_nolib
+cp cybert_vcf_small_fast.py ~/mybin/cybert_vcf_small_fast
