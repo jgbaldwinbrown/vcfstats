c0fd74ca0515ad48365f83204ea1d5104c6e66da
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2020-06-02 11:05:47 -0600

    added combine_all_singles.py, working for generating makefile

--- combine_all_singles.py
+++ combine_all_singles.py
@@ -11,12 +11,12 @@ def get_combos(inconn, vcfin):
             "names_path": sl[0],
             "breed": sl[1],
             "gen": sl[2],
-            "outprefix": sl[3]
-            "names": []
-            "name_regexes": []
+            "outprefix": sl[3],
+            "names": [],
+            "name_regexes": [],
             "name_full_regex": []
         }
-        with open(combo["inpaths_path"], "r") as inpaths_conn:
+        with open(combo["names_path"], "r") as inpaths_conn:
             for l in inpaths_conn:
                 name = l.rstrip('\n')
                 name_regex = "^" + name + "$"
@@ -26,15 +26,36 @@ def get_combos(inconn, vcfin):
         out.append(combo)
     return(out)
 
-def generate_combos(combo, vcfin):
-    vcf_subset_command = "gunzip -c " + vcfin + " | vcf_subset_fast '" + combo["name_full_regex"] + "' | gzip -c > " + outprefix + ".split.vcf.gz"
-    print(vcf_subset_command)
+def generate_combo(combo, vcfin, all_targets, all_commands):
+    splitvcfname = combo["outprefix"] + ".split.vcf.gz"
+    vcf_subset_target = splitvcfname + ": " + vcfin
+    vcf_subset_command = "\tgunzip -c " + vcfin + " | vcf_subset_fast '" + combo["name_full_regex"] + "' | gzip -c > " + splitvcfname
+    all_commands.append(vcf_subset_target)
+    all_commands.append(vcf_subset_command)
+    
+    outname = combo["breed"] + "_" + combo["gen"]
+    outpath = combo["outprefix"] + ".vcf.gz"
+    combine_target = outpath + ": " + splitvcfname
+    combine_command = "\tgunzip -c " + splitvcfname + " | combine_single_indivs_fast -n " + outname + " | gzip -c > " + outpath
+    all_commands.append(combine_target)
+    all_commands.append(combine_command)
+    all_commands.append("")
+    all_targets.append(outpath)
+
+def generate_combos(combos, vcfin):
+    all_targets = []
+    all_commands = []
+    for combo in combos:
+        generate_combo(combo, vcfin, all_targets, all_commands)
+    
+    print("all: " + " ".join(all_targets))
+    print("")
+    print("\n".join(all_commands))
 
 def main():
-    combos_to_do = get_combos(sys.stdin)
     vcfin = sys.argv[1]
-    for combo in combos_to_do:
-        generate_combo(combo, vcfin)
+    combos_to_do = get_combos(sys.stdin, vcfin)
+    generate_combos(combos_to_do, vcfin)
 
 if __name__ == "__main__":
     main()
