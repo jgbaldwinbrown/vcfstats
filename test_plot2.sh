10b63fcaa23d04780b58d726f258f70c83e15cfd
James Baldwin-brown <u6012238@kingspeak2.wasatch.peaks>
2020-11-16 17:28:36 -0700

    updated test_plot2 to use installed plot

--- test_plot2.sh
+++ test_plot2.sh
@@ -1,1 +1,1 @@
-python3 cmh_vcf.py vcf8.txt -C NA00001,NA00002,NA00003 -T NA00004,NA00005,NA00006 | ./plot_vcf_info.py -f NLOG10CMH -o testout2.pdf -x 16,2 -a 0.8
+python3 cmh_vcf.py vcf8.txt -C NA00001,NA00002,NA00003 -T NA00004,NA00005,NA00006 | plot_vcf_info -f NLOG10CMH -o testout2.pdf -x 16,2 -a 0.8
