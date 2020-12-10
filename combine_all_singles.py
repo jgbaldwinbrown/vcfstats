261c5fc47aafb0dd71eb53b9c95a8b758294e837
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2020-12-10 16:41:56 -0700

    working, but not splitting up replicates

--- combine_all_singles.py
+++ combine_all_singles.py
@@ -11,7 +11,8 @@ def get_combos(inconn, vcfin):
             "names_path": sl[0],
             "breed": sl[1],
             "gen": sl[2],
-            "outprefix": sl[3],
+            "treatment": sl[3],
+            "outprefix": sl[4],
             "names": [],
             "name_regexes": [],
             "name_full_regex": []
@@ -29,14 +30,14 @@ def get_combos(inconn, vcfin):
 def generate_combo(combo, vcfin, all_targets, all_commands):
     splitvcfname = combo["outprefix"] + ".split.vcf.gz"
     vcf_subset_target = splitvcfname + ": " + vcfin
-    vcf_subset_command = "\tgunzip -c " + vcfin + " | vcf-subset-fast '" + combo["name_full_regex"] + "' | gzip -c > " + splitvcfname
+    vcf_subset_command = "\tgunzip -c " + vcfin + " | vcf-subset-fast '" + combo["name_full_regex"] + "' | bgzip -c > " + splitvcfname
     all_commands.append(vcf_subset_target)
     all_commands.append(vcf_subset_command)
     
-    outname = combo["breed"] + "_" + combo["gen"]
+    outname = combo["breed"] + "_" + combo["gen"] + "_" + combo["treatment"]
     outpath = combo["outprefix"] + ".vcf.gz"
     combine_target = outpath + ": " + splitvcfname
-    combine_command = "\tgunzip -c " + splitvcfname + " | combine_single_indivs_fast -n " + outname + " | gzip -c > " + outpath
+    combine_command = "\tgunzip -c " + splitvcfname + " | combine_single_indivs_fast -n " + outname + " | bgzip -c > " + outpath
     all_commands.append(combine_target)
     all_commands.append(combine_command)
     all_commands.append("")
