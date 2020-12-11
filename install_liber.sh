06705c888f50907e97811987e3ab097556320043
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2020-12-11 14:10:17 -0700

    install

--- install_liber.sh
+++ install_liber.sh
@@ -28,3 +28,4 @@ cp combine_single_indivs_fast.py ~/mybin/combine_single_indivs_fast
 cp combine_single_indivs.py ~/mybin/combine_single_indivs
 cp vcf2sync.py ~/mybin/vcf2sync
 cp combine_all_singles.py ~/mybin/combine_all_singles
+cp combine_all_singles_replicates.py ~/mybin/combine_all_singles_replicates
