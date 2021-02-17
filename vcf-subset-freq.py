480d022dafa59d3316b00ef5f2b031166ede6c09
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2021-02-17 10:44:08 -0700

    updated freq filter to output bed file as needed and to use argparse

--- vcf-subset-freq.py
+++ vcf-subset-freq.py
@@ -3,19 +3,46 @@
 import sys
 import re
 import subprocess
+import argparse
 
-def parse_header():
+def parse_arguments():
+    parser = argparse.ArgumentParser("Filter VCF file by pool allele frequencies and coverages")
+    parser.add_argument("-f", "--freq", help="Frequency threshold (default=None)", default = None, type=float)
+    parser.add_argument("-c", "--cov", help="Coverage threshold (default=None)", default = None, type=int)
+    parser.add_argument("-b", "--bed_output", help="Output a bed file of all good sites (default=vcf output)", action="store_true")
+    args = parser.parse_args()
+    return(args)
+    # threshold = float(sys.argv[1])
+    # cov_threshold = int(sys.argv[2])
+
+def parse_header(args):
     for l in sys.stdin:
         l=l.rstrip('\n')
-        print(l)
+        if not args.bed_output:
+            print(l)
         if l[:6] == "#CHROM":
             break
 
-def parse_body(threshold, cov_threshold):
+def output_entry(line, args):
+    if not args.bed_output:
+        print(line)
+    else:
+        sl = line.split('\t')
+        print(sl[0], str(int(sl[1])-1), sl[1], sep="\t")
+
+def parse_body(args):
     for l in sys.stdin:
         l = l.rstrip('\n')
         sl = l.split('\t')
         entries = sl[9:]
+        if args.freq:
+            internal_freq_threshold = args.freq
+        else:
+            internal_freq_threshold = -1.0
+        if args.cov:
+            internal_cov_threshold = args.cov
+        else:
+            internal_cov_threshold = -10
         for entry in entries:
             thresh_ok = False
             cov_thresh_ok = True
@@ -28,22 +55,21 @@ def parse_body(threshold, cov_threshold):
                 minor = min(ad1,ad2)
                 tot = ad1 + ad2
                 frac = float(minor) / float(tot)
-                if tot < cov_threshold:
+                if tot < internal_cov_threshold:
                     cov_thresh_ok = False
                     break
-                if frac >= threshold:
+                if frac >= internal_freq_threshold:
                     thresh_ok = True
             except (ZeroDivisionError, ValueError, TypeError, IndexError):
                 error_ok=False
                 break
         if thresh_ok and cov_thresh_ok and error_ok:
-            print(l)
+            output_entry(l, args)
 
 def main():
-    threshold = float(sys.argv[1])
-    cov_threshold = int(sys.argv[2])
-    parse_header()
-    parse_body(threshold, cov_threshold)
+    args = parse_arguments()
+    parse_header(args)
+    parse_body(args)
 
 if __name__ == "__main__":
     main()
